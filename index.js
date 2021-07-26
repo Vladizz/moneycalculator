2e3d6e18cc8334ceba368094573d4805a3a68be4
Valechek1 <79101581094@yandex.ru>
2021-07-26 13:53:50 +0300

    Add structure project

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+const inputName = document.querySelector('.goal-name');
+const inputAmount = document.querySelector('.amount');
+const inputDate = document.querySelector('.date');
+const inputInitialPayment = document.querySelector('.initial-payment');
+const inputInterest = document.querySelector('.interest');
+const addButton = document.querySelector('.btn-add');
+const textGoal = document.querySelector('.text-goal');
+const editBtn = document.querySelector('.btn-edit');
+const deleteBtn = document.querySelector('.btn-delete');
