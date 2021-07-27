61d3d6893220b5fa9425e2017cc535f050db4af0
Valechek1 <79101581094@yandex.ru>
2021-07-27 10:15:36 +0300

    Adding robotic button function

--- index.js
+++ index.js
@@ -4,93 +4,126 @@ const inputDate = document.querySelector('.date');
 const inputInitialPayment = document.querySelector('.initial-payment');
 const inputInterest = document.querySelector('.interest');
 const addButton = document.querySelector('.btn-add');
-// const textGoal = document.querySelector('.text-goal');
-// const editBtn = document.querySelector('.btn-edit');
-// const deleteBtn = document.querySelector('.btn-delete');
+const replenishmentPercentage = document.querySelector('.replenishment-percentage');
 
-const boxText = document.querySelector('.box-text')
+const guid = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+  return (
+    `${s4()
+    + s4()
+    }-${s4()
+    }-${s4()
+    }-${s4()
+    }-${s4()
+    }${s4()
+    }${s4()}`
+  );
+};
 
+function searchInAnArray(array, id) {
+  const data = array.find((el) => el.id === id);
+  inputName.value = data.name;
+  inputAmount.value = data.amount;
+  inputDate.value = data.date;
+  inputInitialPayment.value = data.payment;
+  inputInterest.value = data.interest;
+}
+
+const boxText = document.querySelector('.box-text');
 const massivFromFirstBlock = [];
+function getUserData() {
+  const userData = {
+    id: guid(),
+    name: inputName.value,
+    amount: inputAmount.value,
+    date: inputDate.value,
+    payment: inputInitialPayment.value,
+    interest: inputInterest.value,
+  };
+  return userData;
+}
 
-function getUserData () {
+function zeroingForm() {
+  inputName.value = '';
+  inputAmount.value = '';
+  inputDate.value = '';
+  inputInitialPayment.value = '';
+  inputInterest.value = '';
+}
 
-  const userData  = {
-      name: inputName.value,
-      amount: inputAmount.value,
-      date: inputDate.value,
-      payment: inputInitialPayment.value,
-      interest: inputInterest.value
-    };
+function showMyGoal(goal) {
+  const goalElDiv = document.createElement('div');
+  goalElDiv.classList.add('goal');
 
-  console.log(massivFromFirstBlock);
+  const editButton = document.createElement('button');
+  editButton.classList.add('btn-edit');
 
-  return userData
+  const editPic = document.createElement('img');
+  editPic.src = 'images/edit-btn.svg';
+  editPic.classList.add('edit-img');
+
+  editButton.append(editPic);
+
+  const myGoalMetrics = document.createElement('div');
+  myGoalMetrics.classList.add('text-goal');
+
+  const nameP = document.createElement('h3');
+  nameP.classList.add('p-design');
+  nameP.innerText = `Ваша цель: ${goal.name}`;
+
+  const amountP = document.createElement('p');
+  amountP.classList.add('p-design');
+  amountP.innerText = `Требуемая сумма: ${goal.amount}`;
+
+  const dateP = document.createElement('p');
+  dateP.classList.add('p-design');
+  dateP.innerText = `Срок: ${goal.date}`;
+
+  const paymentP = document.createElement('p');
+  paymentP.classList.add('p-design');
+  paymentP.innerText = `Стартовая сумма: ${goal.payment}`;
+
+  const interestP = document.createElement('p');
+  interestP.classList.add('p-design');
+  interestP.innerText = `Процент: ${goal.interest}`;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('btn-delete');
+
+  const deletePic = document.createElement('img');
+  deletePic.src = 'images/delete-btn.svg';
+  deletePic.classList.add('delete-img');
+
+  deleteButton.append(deletePic);
+  goalElDiv.append(editButton);
+  goalElDiv.append(myGoalMetrics);
+  myGoalMetrics.append(nameP);
+  myGoalMetrics.append(amountP);
+  myGoalMetrics.append(dateP);
+  myGoalMetrics.append(paymentP);
+  myGoalMetrics.append(interestP);
+  goalElDiv.append(deleteButton);
+  boxText.append(goalElDiv);
+
+  editButton.addEventListener('click', () => {
+    searchInAnArray(massivFromFirstBlock, goal.id);
+    goalElDiv.remove();
+  });
+
+  deleteButton.addEventListener('click', () => {
+    goalElDiv.remove();
+    const dataIndex = massivFromFirstBlock.findIndex((el) => el.id === goal.id);
+    massivFromFirstBlock.slice(dataIndex, 1);
+  });
 }
 
 addButton.addEventListener('click', () => {
-
-  const data = getUserData()
-
+  const data = getUserData();
   massivFromFirstBlock.push(data);
-
-  showMyGoal(data)
-
+  showMyGoal(data);
   getUserData();
-  inputName.value ="";
-  inputAmount.value ="";
-  inputDate.value ="";
-  inputInitialPayment.value ="";
-  inputInterest.value ="";
+  zeroingForm();
+  console.log(data);
 });
-
-function showMyGoal (goal) {
-  let goalElDiv = document.createElement('div')
-  goalElDiv.classList.add('goal')
-
-  let editButton = document.createElement('button')
-  editButton.classList.add('btn-edit')
-  let editPic = document.createElement('img')
-  editPic.src = 'images/edit-btn.svg'
-  editPic.classList.add('edit-img')
-  
-  editButton.append(editPic)
-
-  let myGoalMetrics = document.createElement('div')
-  myGoalMetrics.classList.add('text-goal')
-  let nameP = document.createElement('h3')
-  nameP.classList.add('p-design')
-  nameP.innerText = `Ваша цель: ${goal.name}`
-  let amountP = document.createElement('p')
-  amountP.classList.add('p-design')
-  amountP.innerText = `Требуемая сумма: ${goal.amount}`
-  let dateP = document.createElement('p')
-  dateP.classList.add('p-design')
-  dateP.innerText = `Срок: ${goal.date}`
-  let paymentP = document.createElement('p')
-  paymentP.classList.add('p-design')
-  paymentP.innerText = `Стартовая сумма: ${goal.payment}`
-  let interestP = document.createElement('p')
-  interestP.classList.add('p-design')
-  interestP.innerText = `Процент: ${goal.interest}`
-
-  let deleteButton = document.createElement('button')
-  deleteButton.classList.add('btn-delete')
-  let deletePic = document.createElement('img')
-  deletePic.src = 'images/delete-btn.svg'
-  deletePic.classList.add('delete-img')
-
-  deleteButton.append(deletePic)
-
-  goalElDiv.append(editButton)
-  goalElDiv.append(myGoalMetrics)
-
-  myGoalMetrics.append(nameP)
-  myGoalMetrics.append(amountP)
-  myGoalMetrics.append(dateP)
-  myGoalMetrics.append(paymentP)
-  myGoalMetrics.append(interestP)
-
-  goalElDiv.append(deleteButton)
-
-  boxText.append(goalElDiv)
-}
